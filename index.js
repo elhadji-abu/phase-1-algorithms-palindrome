fa3fd85bf1289fe1c28ecc2ec0a3eece052b7dea
elhadji-abu <deenowbudi@gmail.com>
2024-06-08 09:30:12 +0300

    complete palindrome lab

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+    word = word.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  
+    for (let i = 0; i < Math.floor(word.length / 2); i++) {
+      if (word[i] !== word[word.length - i - 1]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  
+  module.exports = isPalindrome;
